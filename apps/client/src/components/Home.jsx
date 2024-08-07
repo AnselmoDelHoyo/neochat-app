f1f47806fa613f57001ae3dd03ad56a7c451428d
Anselmo1122 <anselmodelhoyo11@gmail.com>
2024-08-07 19:12:25 +0100

    Add(client): finished version one home page

--- apps/client/src/components/Home.jsx
+++ apps/client/src/components/Home.jsx
@@ -1,11 +1,13 @@
 // import React from 'react'
-import './Home.css'
+import '../styles/Home.css'
 
 export default function Home() {
     return (
         <main id='home'>
-          <p>This is neochat, a web chat application with a epic design.</p>
-          <h1 className='home__title'>Neochat</h1>
+          <div className='home__text'>
+            <p>This is neochat, a web chat application with a epic design.</p>
+          </div>
+          <h1 className='home__title'><span className='title__branch'>Neo</span>chat</h1>
           <section className='home__buttons'>
             <button>Sign In</button>
             <button>Sign Up</button>
